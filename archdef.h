ca0e7903992be1c038b25f8c92154d488b242aee
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2021-06-26 11:21:27 +0200

    init

---
+++ archdef.h
@@ -0,0 +1,16 @@
+#ifndef VV_ARCHDEF_H
+#define VV_ARCHDEF_H
+
+#include <cstdint>
+
+#define int8 int8_t
+#define int16 int16_t
+#define int32 int32_t
+#define int64 int64_t \
+                      \
+#define uint8 uint8_t
+#define uint16 uint16_t
+#define uint32 uint32_t
+#define uint64 uint64_t
+
+#endif //VV_ARCHDEF_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+#include "archdef.h"
+
+INT WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
+                   PSTR CommandLine, INT ShowCode)
+{
+    return 0;
+}
